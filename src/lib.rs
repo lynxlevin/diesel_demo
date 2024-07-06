7023d393ee748afc1063227705f17d9d05108e69
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2024-07-06 14:03:53 +0900

    Write_post

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
-use diesel::{Connection, PgConnection};
+pub mod models;
+pub mod schema;
+
+use diesel::prelude::*;
 use dotenvy::dotenv;
 use std::env;
 
-pub mod models;
-pub mod schema;
+use self::models::{NewPost, Post};
 
 pub fn establish_connection() -> PgConnection {
     dotenv().ok();
@@ -11,4 +13,16 @@ pub fn establish_connection() -> PgConnection {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     PgConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+}
+
+pub fn create_post(conn: &mut PgConnection, title: &str, body: &str) -> Post {
+    use crate::schema::posts;
+
+    let new_post = NewPost { title, body };
+
+    diesel::insert_into(posts::table)
+        .values(&new_post)
+        .returning(Post::as_returning())
+        .get_result(conn)
+        .expect("Error saving new post")
 }
